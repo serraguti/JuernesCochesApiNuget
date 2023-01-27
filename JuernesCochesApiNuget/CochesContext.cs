0a0e45f6882438ab5734240f2dea488de5d8a8c1
serraguti <pacoserranox@gmail.com>
2023-01-27 16:27:28 +0100

    Add project files.

---
+++ JuernesCochesApiNuget/CochesContext.cs
@@ -0,0 +1,26 @@
+﻿using JuernesCochesApiNuget.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuernesCochesApiNuget
+{
+    public class CochesContext
+    {
+        public async Task<List<Coche>> GetCochesAsync()
+        {
+            WebClient client = new WebClient();
+            client.Headers["content-type"] = "application/json";
+            string urlApi = "https://apicochespaco.azurewebsites.net/webresources/coches";
+            string json =
+                await client.DownloadStringTaskAsync(urlApi);
+            List<Coche> coches =
+                JsonConvert.DeserializeObject<List<Coche>>(json);
+            return coches;
+        }
+    }
+}
---
+++ JuernesCochesApiNuget/Models/Coche.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuernesCochesApiNuget.Models
+{
+    public class Coche
+    {
+        [JsonProperty("idcoche")]
+        public int IdCoche { get; set; }
+        [JsonProperty("marca")]
+        public string Marca { get; set; }
+        [JsonProperty("modelo")]
+        public string Modelo { get; set; }
+        [JsonProperty("conductor")]
+        public string Conductor { get; set; }
+        [JsonProperty("imagen")]
+        public string Imagen { get; set; }
+    }
+}
